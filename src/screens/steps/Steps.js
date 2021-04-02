514345c7f08a4a0eba060b0e80ab671c470286b2
Maria Luisa <maria.luisa@thoughtworks.com>
2021-04-02 01:12:15 -0300

    @marialuisacp init project

---
+++ src/screens/steps/Steps.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+const Steps = () => {
+  return <View>
+    <Text> Teste </Text>
+  </View>
+};
+
+export default Steps;
