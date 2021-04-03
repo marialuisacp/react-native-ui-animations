954dc81b8c5145f76d42fa3e0ad6aa69f0fbfeea
Maria Luisa <maria.luisa@thoughtworks.com>
2021-04-03 16:43:49 -0300

    @marialuisacp adding new images and creating common styles

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,2 +1,3 @@
 export const BG_STEPS = require('./images/bg_steps.png');
+export const BG_LOGIN = require('./images/bg_login.png');
 export const LOGO_INITIAL = require('./images/logo_initial.png');
