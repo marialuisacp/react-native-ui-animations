f19577bfaa16620eeb1d0dda6905d3279f5f41bf
Maria Luisa <maria.luisa@thoughtworks.com>
2021-04-03 17:25:15 -0300

    @marialuisacp adding new images

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,3 +1,6 @@
 export const BG_STEPS = require('./images/bg_steps.png');
 export const BG_LOGIN = require('./images/bg_login.png');
 export const LOGO_INITIAL = require('./images/logo_initial.png');
+export const IMAGE_CLOCK = require('./images/image_clock.png');
+export const IMAGE_LIGHT_BULB = require('./images/image_light_bulb.png');
+export const IMAGE_CALENDAR = require('./images/image_calendar.png');
